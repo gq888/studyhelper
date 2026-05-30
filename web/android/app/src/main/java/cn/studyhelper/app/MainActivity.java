0497ef86f3f712bee964c869185e01085bbb099e
gq888 <li1005345206@qq.com>
2026-05-30 17:29:58 +0800

    fix: 回滚 MainActivity 修改，Capacitor 8 不需要手动注册插件

--- web/android/app/src/main/java/cn/studyhelper/app/MainActivity.java
+++ web/android/app/src/main/java/cn/studyhelper/app/MainActivity.java
@@ -1,19 +1,5 @@
 package cn.studyhelper.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ArrayList<Class<? extends Plugin>> additionalPlugins = new ArrayList<>();
-        additionalPlugins.add(com.getcapacitor.app.App.class);
-        additionalPlugins.add(com.getcapacitor.splashscreen.SplashScreen.class);
-        additionalPlugins.add(com.getcapacitor.statusbar.StatusBar.class);
-        additionalPlugins.add(com.getcapacitor.localnotifications.LocalNotifications.class);
-        registerPlugins(additionalPlugins);
-    }
-}
+public class MainActivity extends BridgeActivity {}
